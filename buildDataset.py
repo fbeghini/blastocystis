cac0dd8a5a0d32c682fade51945d59a5105b186c
Francesco Beghini <fbeghini@outlook.com>
2015-03-28 18:14:20 +0100

    buildDataset: Completed.

--- buildDataset.py
+++ buildDataset.py
@@ -1,33 +1,27 @@
 #! /usr/bin/env python
-import os, argparse, gzip, bz2  
+import os, argparse, gzip  
 from Bio import SeqIO
+from bz2 import BZ2File
+from tempfile import NamedTemporaryFile
 
 parser = argparse.ArgumentParser()
-parser.add_argument("input-folder", help="Location of the dataset")
+parser.add_argument("inputfolder", help="Location of the dataset")
 parser.add_argument("name", help="Name of the output file")
 
 args = parser.parse_args()
 
 #Build fasta dataset
-if(os.path.isdir(args.input_folder)):
-    with open(name,"a") as out:
-        for (p,d,f) in os.walk(args.input_folder):
-            extention = os.path.splitext(f)[1]
-             if any(f in extention for f in ["fasta","fa","fna"]):
-                    handle = open(path+"/"+g,"r")
-            elif 'gz' in extention:
-                    handle = gzip.open(path+"/"+g)
-            elif "bz2" in extention:
-                    handle = BZ2File(path+"/"+g)
+if(os.path.isdir(args.inputfolder)):
+	with NamedTemporaryFile(delete=True) as _out:
+		for (path,dirs,files) in os.walk(args.inputfolder):
+			for f in files:
+				extention = os.path.splitext(f)[1]
+				if any(f in extention for f in ["fasta","fa","fna"]):
+						handle = open(path+"/"+f,"r")
+				elif 'gz' in extention:
+						handle = gzip.open(path+"/"+f)
+				elif "bz2" in extention:
+						handle = BZ2File(path+"/"+f)
+				_out.writelines(handle.readlines())
 
-            out.write(handle.readlines())
-
-#Convert dataset
-
-SeqIO.convert(name,"fasta",name+".conv","fasta")
-os.remove(name)
-os.rename(name+".conv",name)
-try:
-    SeqIO.convert("","fasta","","fasta")
-except:
-    pass
+		SeqIO.convert(_out.name,"fasta",args.name,"fasta")
